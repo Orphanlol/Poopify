86439369314c0ce169db9974b34aea5f755b4288
Orphanlol <83834491+Orphanlol@users.noreply.github.com>
2023-10-05 17:55:45 -0400

    nut hack

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-const replaceScript = "https://cdn.i-ready.com/instruction/student-dashboard/release-13.5.x/2/static/js/app-21.chunk.js";
-if (document.scripts[12].src == replaceScript) {
+'use strict';
+if (document.scripts[12].src == "https://cdn.i-ready.com/instruction/student-dashboard/release-13.5.x/2/static/js/app-21.chunk.js") {
   alert("lesson has already been loaded, refresh the page and run before going into your lesson");
 } else {
-  fetch("https://raw.githubusercontent.com/Orphanlol/Poopify/main/app-21.chunk.js").then(_0x14eac6 => _0x14eac6.text()).then(_0x186679 => eval(_0x186679));
+  fetch("https://raw.githubusercontent.com/Orphanlol/Poopify/main/app-21.chunk.js").then((mElmOrSub) => {
+    return mElmOrSub.text();
+  }).then((_0x186679$jscomp$0$jscomp$0$jscomp$0) => {
+    return eval(_0x186679$jscomp$0$jscomp$0$jscomp$0);
+  });
 }
-window.yes = function () {
+window.yes = function() {
   p1(prompt("score?"));
 };
-setInterval(function () {
+setInterval(function() {
   try {
     if (html5Iframe.contentWindow.document.getElementById("nav-forward") != undefined) {
       html5Iframe.contentWindow.document.getElementById("nav-forward").disabled = false;
       html5Iframe.contentWindow.document.getElementById("nav-forward").onclick = yes;
     }
-  } catch (_0x5c4082) {}
+  } catch (_0x5c4082) {
+  }
 }, 10);
