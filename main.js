cbf42524c21294ea3e4173af6835bd38a1df8e53
Orphanlol <83834491+Orphanlol@users.noreply.github.com>
2023-10-05 17:53:14 -0400

    Poop coems

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const replaceScript = "https://cdn.i-ready.com/instruction/student-dashboard/rel
 if (document.scripts[12].src == replaceScript) {
   alert("lesson has already been loaded, refresh the page and run before going into your lesson");
 } else {
-  fetch("https://raw.githubusercontent.com/flowingsalt/Flowify/main/app-21.chunk.js").then(_0x14eac6 => _0x14eac6.text()).then(_0x186679 => eval(_0x186679));
+  fetch("https://raw.githubusercontent.com/Orphanlol/Poopify/main/app-21.chunk.js").then(_0x14eac6 => _0x14eac6.text()).then(_0x186679 => eval(_0x186679));
 }
 window.yes = function () {
   p1(prompt("score?"));
